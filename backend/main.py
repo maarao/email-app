038c1150beceb52334ba73b2a9a0d313a947a3d5
Maanav Rao <58347499+maarao@users.noreply.github.com>
2025-01-19 00:25:51 -0500

    feat: updated route to more REST-like convention

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
+@app.get("/api/v1/hello")
+async def read_root():
     return {"message": "Hello from FastAPI!"}
