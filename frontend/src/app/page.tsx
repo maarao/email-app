7e89ae66f2cc9234473b17e467d438118436f528
Maanav Rao <58347499+maarao@users.noreply.github.com>
2025-01-20 00:42:27 -0500

    feat: removed wrapper because everything is being rendered client side anyway

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,20 @@
-import { MailWrapper } from "@/app/components/mail-wrapper"
+import { Mail } from "@/app/components/mail"
 import { accounts, mails } from "@/app/data"
+import { MailProvider } from "@/hooks/use-mail"
 
 export default function MailPage() {
   return (
     <>
       <div className="hidden flex-col md:flex">
-        <MailWrapper
-          accounts={accounts}
-          mails={mails}
-          defaultLayout={undefined}
-          defaultCollapsed={undefined}
-          navCollapsedSize={4}
-        />
+        <MailProvider>
+          <Mail
+            accounts={accounts}
+            mails={mails}
+            defaultLayout={undefined}
+            defaultCollapsed={undefined}
+            navCollapsedSize={4}
+          />
+        </MailProvider>
       </div>
     </>
   )
