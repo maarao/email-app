708975353a66f49cdebaaf05eb288f0a277c49a2
Maanav Rao <58347499+maarao@users.noreply.github.com>
2025-01-20 00:39:18 -0500

    feat: removed mobile block that didn't even work

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,9 @@
-import Image from "next/image"
 import { MailWrapper } from "@/app/components/mail-wrapper"
 import { accounts, mails } from "@/app/data"
 
 export default function MailPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </div>
       <div className="hidden flex-col md:flex">
         <MailWrapper
           accounts={accounts}
